6157001eeca57459980aaef8c322b50897b41e06
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-10 09:53:49 +0100

    dodat navbar

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    baseUrl:'http://localhost:8080/todo/todo-backend'
+  },
   mutations: {},
   actions: {},
   modules: {}
---
+++ todo-frontend/src/views/ListView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Navbar/>
+        <List/>
+
+    </div>
+</template>
+<script>
+
+import Navbar from "@/components/Navbar.vue";
+
+import List from "@/components/List.vue";
+
+export default{
+    components:{
+        Navbar,
+        List
+    }
+}
+
+</script>
+<style scoped>
+</style>
